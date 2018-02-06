8ef465eaef7224765148bd2705ac976280da9cee
Highlevelyuri <kriucov61944zce3.com@gmail.com>
2018-02-06 12:33:32 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+int an = 8 ;
+        int sum;
+        int a1 =2;
+     for (int b=2; b <=30; b+=4){
+         System.out.println(b);
+
+     }
+        sum = ((a1+ an)/2)* 8;
+        System.out.println( "\n Sum:" + sum);
+
+    }
+}
+
+
+
+
+
+
+
+
+
